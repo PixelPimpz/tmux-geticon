35499ece60c8ab159b5a5ac1beb87b8195a28356
lucifer-morningstar <slobeck@gmail.com>
2025-12-26 15:14:21 -0800

    Generic commit

--- scripts/tmux-geticon.sh
+++ scripts/tmux-geticon.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
 tmux display -p "tmux-geticon.sh running..."
+[[ ! -x /usr/bin/yq ]] && fatal "\"yq\" executable not found."
+ICON="$1"
+main() {
+  local icons="$(tmux display -p "#{@LIB_ICON}")"
+  local icon="$(yq e '.icons.[].$ICON' "${icons}" )" 
+        
+  echo ${icon}
+}
+
+fatal() {
+  local e="$1"
+  printf 'ERROR: %s\n' "${e}"
+  exit 1
+}
