c278cb8cfe0e82179d3f06cbb08d47e8860e510e
lucifer-morningstar <slobeck@gmail.com>
2025-12-25 17:34:25 -0800

    basic push. code/fizes

--- scripts/tmux-geticon.sh
+++ scripts/tmux-geticon.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+usage="${0##*/}"
+tmux setenv '@PLUG_ROOT' "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$PLUG_ROOT="$(tmux display -p "#{@PLUG_ROOT}")"
+$ICONS="$(tmux display -p "#{@LIB_ICON}")"
+tmux display "#[align=centre]tmux-geticon.sh running..."
+$PLUG_ROOT/scripts/tmux-metaphile.sh
+tmux bind M-I run "$PLUG_ROOT/tmux-geticon"
