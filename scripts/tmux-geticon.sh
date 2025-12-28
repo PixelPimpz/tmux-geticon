e668a74bf871e1a51df14cd09bfd8f0c53a2cfed
lucifer-morningstar <slobeck@gmail.com>
2025-12-27 20:12:45 -0800

    Generic commit

--- scripts/tmux-geticon.sh
+++ scripts/tmux-geticon.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
+ICON="$1"
 DEBUG=1
-
+ICONS='$(tmux display -p \"#{@LIB_ICON}\")'
+(( DEBUG == 1 )) && tmux display -p "tmux-geticon.sh running..."
 [[ ! -x /usr/bin/yq ]] && fatal "\"yq\" executable not found."
-[[ -n "$1" ]] && ICON="$1" || ICON="default"
+[[ ! -n "$ICON" ]] && ICON="default"
 
 main() {
-  local icons="$(tmux display -p "#{@LIB_ICON}")"
-  local icon="$(yq ".icons.[].$ICON" "${icons}" | grep -v "null" )" 
+  local icon="$(yq ".icons.[].$ICON" "${ICONS}" | grep -v "null" )" 
   (( $? != 0 )) && fatal "yq failed to get icon. Check paths and config"        
-  if (( DEBUG != 0 )); then
-    tmux display -p "tmux-geticon.sh running..."
-    tmux display -p ">> ICON: $ICON"
-    tmux display -p ">> Icon: ${icon}"
-  fi
   result "$icon" 
 }
 
 result() {
-  local icon="$1"
-  echo "${icon}"
+  local ico="$1"
+  echo "${ico}"
 }
 
 fatal() {
